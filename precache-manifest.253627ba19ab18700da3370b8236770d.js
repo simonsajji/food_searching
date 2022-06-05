f58f241898791baaf0564cbd9b04f43346c1af46
simonsajji <simonmathai777@gmail.com>
2022-06-05 18:45:39 +0530

    Updates

---
+++ precache-manifest.253627ba19ab18700da3370b8236770d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2ebf42eee618ec5a6e4cbbc51e1551",
+    "url": "/food_searching/index.html"
+  },
+  {
+    "revision": "9259f758971d7e3dbf05",
+    "url": "/food_searching/static/css/main.093836e1.chunk.css"
+  },
+  {
+    "revision": "d310b8de8575d8f79fa3",
+    "url": "/food_searching/static/js/2.dd023780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food_searching/static/js/2.dd023780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9259f758971d7e3dbf05",
+    "url": "/food_searching/static/js/main.3fbf850b.chunk.js"
+  },
+  {
+    "revision": "a25e0ed03ea08b030010",
+    "url": "/food_searching/static/js/runtime-main.07270448.js"
+  }
+]);
